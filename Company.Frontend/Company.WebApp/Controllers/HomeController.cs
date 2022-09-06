422819f39068d9b20b4e129188686aa33a1213ba
gorohov09 <andrej.gorokhoff2017@yandex.ru>
2022-09-06 23:07:28 +0300

    Добавление пакетов для фронтенд части

--- Company.Frontend/Company.WebApp/Controllers/HomeController.cs
+++ Company.Frontend/Company.WebApp/Controllers/HomeController.cs
@@ -1,6 +1,4 @@
-﻿using Company.WebApp.Models;
-using Microsoft.AspNetCore.Mvc;
-using System.Diagnostics;
+﻿using Microsoft.AspNetCore.Mvc;
 
 namespace Company.WebApp.Controllers
 {
@@ -16,17 +14,6 @@ namespace Company.WebApp.Controllers
         public IActionResult Index()
         {
             return View();
-        }
-
-        public IActionResult Privacy()
-        {
-            return View();
-        }
-
-        [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
-        public IActionResult Error()
-        {
-            return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
         }
     }
 }
