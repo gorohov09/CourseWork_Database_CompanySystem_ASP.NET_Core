ccf2793917311c4f45aeeb1c6a3f7558280b56e5
gorohov09 <andrej.gorokhoff2017@yandex.ru>
2022-09-07 18:12:36 +0300

    Создание базовой сущности и сущности - сотрудник

---
+++ Company.Backend/Core/Company.Domain/Base/Entity.cs
@@ -0,0 +1,13 @@
+﻿using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace Company.Domain.Base
+{
+    public class Entity
+    {
+        [Key]
+        [Required]
+        [DatabaseGenerated(DatabaseGeneratedOption.Identity)]
+        public int Id { get; set; }
+    }
+}
---
+++ Company.Backend/Core/Company.Domain/Entities/EmployeeEntity.cs
@@ -0,0 +1,24 @@
+﻿using Company.Domain.Base;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace Company.Domain.Entities
+{
+    public class EmployeeEntity : Entity
+    {
+        public string LastName { get; set; }
+
+        public string FirstName { get; set; }
+
+        public string? Patronymic { get; set; }
+
+        public DateTime Birthday { get; set; }
+
+        public long PhoneNumber { get; set; }
+
+        public string Email { get; set; }
+
+        [Column(TypeName = "decimal(18,2)")] //Указание типа, который храним в БД
+        public decimal Salary { get; set; }
+    }
+}
