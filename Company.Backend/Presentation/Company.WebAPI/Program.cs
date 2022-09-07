81b663a4fd16e20d42ed7a21d9541669ffca6a5d
gorohov09 <andrej.gorokhoff2017@yandex.ru>
2022-09-07 18:55:09 +0300

    Определение строки подключения + добавление контекста в контейнер сервисов

--- Company.Backend/Presentation/Company.WebAPI/Program.cs
+++ Company.Backend/Presentation/Company.WebAPI/Program.cs
@@ -1,11 +1,19 @@
+using Company.DAL.Context;
+using Microsoft.EntityFrameworkCore;
+
 var builder = WebApplication.CreateBuilder(args);
 
-// Add services to the container.
+var services = builder.Services;
 
-builder.Services.AddControllers();
-// Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
-builder.Services.AddEndpointsApiExplorer();
-builder.Services.AddSwaggerGen();
+services.AddControllers();
+
+services.AddEndpointsApiExplorer();
+services.AddSwaggerGen();
+
+services.AddDbContext<CompanyContext>(options =>
+{
+    options.UseSqlServer(builder.Configuration.GetConnectionString("DefaultConnection"));
+});
 
 var app = builder.Build();
 
