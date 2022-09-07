5bb2e18dca6ee8bbcc0132c63105ca0f2ab5f283
gorohov09 <andrej.gorokhoff2017@yandex.ru>
2022-09-07 18:20:02 +0300

    Создание контекста БД

---
+++ Company.Backend/Infrastructure/Company.DAL/Context/CompanyContext.cs
@@ -0,0 +1,16 @@
+﻿using Company.Domain.Entities;
+using Microsoft.EntityFrameworkCore;
+
+namespace Company.DAL.Context
+{
+    public class CompanyContext : DbContext
+    {
+        public DbSet<EmployeeEntity> Employees { get; set; }
+
+        public CompanyContext(DbContextOptions<CompanyContext> options)
+            : base(options)
+        {
+
+        }
+    }
+}
